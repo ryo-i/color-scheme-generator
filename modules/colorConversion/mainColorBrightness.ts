d572d8142aef27d1cc494dff0738288871f519c9
ryo-i <iidazz0109@gmail.com>
2021-07-21 07:19:22 +0900

    ベースカラー→メインカラーの書式整理 #23

--- modules/colorConversion/mainColorBrightness.ts
+++ modules/colorConversion/mainColorBrightness.ts
@@ -1,25 +1,23 @@
 const mainColorBrightness = (contrast: number, brightness: number) => {
-    let mainColorBrightness: number = 0;
+    let resultContrast: number = 0;
+    let resultBrightness: number = 0;
+
+    if (contrast == 0) {
+        resultBrightness = brightness;
+    } else if (contrast < 0) {
+        resultBrightness = (brightness - contrast) / (1 - contrast / 100);
+    } else if (contrast > 0) {
+        resultBrightness = brightness / (1 - contrast / 100);
+    }
 
     const result: {
         contrast: number,
         brightness: number
     } = {
         contrast: contrast,
-        brightness: brightness
+        brightness: Math.round(resultBrightness)
     };
 
-    if (contrast == 0) {
-        mainColorBrightness = brightness;
-    } else if (contrast < 0) {
-        // mainColorBrightness = (brightness + contrast) / (1 + (contrast / 100))
-        mainColorBrightness = (brightness - contrast) / (1 - contrast / 100);
-    } else if (contrast > 0) {
-        // mainColorBrightness = brightness / (1 - (contrast / 100))
-        mainColorBrightness = brightness / (1 - contrast / 100);
-    }
-
-    result.brightness = Math.round(mainColorBrightness);
     return result;
 }
 
--- modules/colorConversion/mainColorSaturation.ts
+++ modules/colorConversion/mainColorSaturation.ts
@@ -1,23 +1,23 @@
 const mainColorSaturation = (contrast: number, saturation: number) => {
-    let mainColorSaturation: number = 0;
+    let resultContrast: number = 0;
+    let resultSaturation: number = 0;
+
+    if (contrast == 0) {
+        resultSaturation = saturation;
+    } else if (contrast < 0) {
+        resultSaturation = saturation / (1 - contrast / 100);
+    } else if (contrast > 0) {
+        resultSaturation = (saturation - contrast) / (1 - contrast / 100);
+    }
 
     const result: {
         contrast: number,
         saturation: number
     } = {
         contrast: contrast,
-        saturation: saturation
+        saturation: Math.round(resultSaturation)
     };
 
-    if (contrast == 0) {
-        mainColorSaturation = saturation;
-    } else if (contrast < 0) {
-        mainColorSaturation = saturation / (1 - contrast / 100);
-    } else if (contrast > 0) {
-        mainColorSaturation = (saturation - contrast) / (1 - contrast / 100);
-    }
-
-    result.saturation = Math.round(mainColorSaturation);
     return result;
 }
 
